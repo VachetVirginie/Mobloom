be6f1b219004ea7ab4335f4fd3c79acdb6d8bf74
Vivi <vachetvirginie@gmail.com>
2023-03-21 17:37:34 +0100

    fix header style

--- src/components/Base/HeaderBase.vue
+++ src/components/Base/HeaderBase.vue
@@ -1,10 +1,10 @@
 <template>
   <header v-if="route !== 'Login'">
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800">
+    <nav class="border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800">
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <a href="/home" class="flex items-center">
           <img src="@/assets/psw.png" class="mr-3 h-6 sm:h-9" alt="Mobloom Logo" />
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Mobloom</span>
+          <span class="self-center text-xl font-semibold whitespace-nowrap text-white">Mobloom</span>
         </a>
         <div class="flex items-center lg:order-2">
           <a @click="logOutSession" class="text-gray-800 text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800">Log out</a>
