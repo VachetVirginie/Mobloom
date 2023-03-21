9050f2e8911fee298536ca2bcb93d275b1bc0ede
Vivi <vachetvirginie@gmail.com>
2023-03-21 17:23:38 +0100

    wip add .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseApp = initializeApp({
-    apiKey: "AIzaSyDtDfAUEvne4vge8siJS04p8kwk_pZ6s1E",
-    authDomain: "mym2ps.firebaseapp.com",
-    projectId: "mym2ps",
-    storageBucket: "mym2ps.appspot.com",
-    messagingSenderId: "810157837152",
-    appId: "1:810157837152:web:b7160c25b0088464a6ac05",
-    measurementId: "G-11CY4K0Y7P"
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 });
 
 export const auth = getAuth(firebaseApp);
