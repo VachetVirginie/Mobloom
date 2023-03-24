77df61fb130820446f8ef3c75fa7c4c66279217c
Vivi <vachetvirginie@gmail.com>
2023-03-24 12:01:22 +0100

    fix disallow test in useAos

--- src/composables/useAos.js
+++ src/composables/useAos.js
@@ -3,10 +3,10 @@ import 'aos/dist/aos.css';
 
 
 export const aos = app.AOS = new AOS.init({
-    disable: () => {
-        let maxWidth = 1000;
-        return window.innerWidth < maxWidth;
-    },
+    // disable: () => {
+    //     let maxWidth = 1000;
+    //     return window.innerWidth < maxWidth;
+    // },
     once: true,
     duration: 700
 });
